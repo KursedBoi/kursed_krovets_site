a926add28710fece5564524e66b9508f69efa2e5
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2022-02-27 16:54:27 -0800

    added explanations from mimir for hw questions

---
+++ final_files/final_mimir_2.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bn152.h"
+
+// Return 0 if x>=p, x<1, or p not prime.
+// Otherwise return 1 and write x's inverse to res
+int find_inverse(BIGNUM *res, BIGNUM *x, BIGNUM *p) 
+{
+
+}
